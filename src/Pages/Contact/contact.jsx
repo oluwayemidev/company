11d3fed417d14d03d7d3832b24a2ac16c712f0c1
Oluwayemi <lordryefox@gmail.com>
2022-10-26 14:28:05 +0100

    contact

--- src/Pages/Contact/contact.jsx
+++ src/Pages/Contact/contact.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "../../styles/contact.css"
+import contactbg from "../../images/contactbg1.jpg"
 
 function contact() {
   return (
-    <div>contact</div>
+    <div className='container-contact' style={{
+      backgroundImage: `url(${contactbg})`
+    }}>
+      <div className="contactbox">
+        <p>Contact Us</p>
+        <input type="text" name="" id="" placeholder='Name' />
+        <input type="text" name="" id="" placeholder='Email' />
+        <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+        <button>Submit</button>
+      </div>
+    </div>
   )
 }
 
