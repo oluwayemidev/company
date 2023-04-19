f2c936dc6676b5ed98f32171e3b25b0c193e3c68
Oluwayemi <lordryefox@gmail.com>
2023-04-19 19:49:53 +0100

    form done

--- src/Pages/Form/login.jsx
+++ src/Pages/Form/login.jsx
@@ -1,8 +1,35 @@
+import "../../styles/form.css"
+import banner from "../../images/lapi.jpg"
 import React from 'react'
+import user from "../../icons/user.svg"
+import mail from "../../icons/mail.svg"
+import password from "../../icons/password.svg"
 
 function login() {
   return (
-    <div>login</div>
+    <div className="container-form">
+        <div className="box" style={{
+      backgroundImage: `url(${banner})`
+  }}>
+          <div>
+              <h1 className="h1login">g in</h1>
+              <form action="">
+                <input type="epassword" name="email" placeholder="Enter your email" style={{
+                    backgroundImage: `url(${mail})`
+                }} />
+                <input type="password" name="password" placeholder="Enter your password" style={{
+                    backgroundImage: `url(${password})`
+                }} />
+                <a href="/forgotpassword">Forgot password?</a>
+                <input type="submit" value="Log in" />
+                <span>Don't have an account? <a href="/signup">Sign up</a></span>
+              </form>
+          </div>
+          <div>
+            <button>Welcome...</button>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- src/Pages/Form/signup.jsx
+++ src/Pages/Form/signup.jsx
@@ -1,17 +1,35 @@
 import "../../styles/form.css"
-import bg from "../../images/contactbg1.jpg"
+import banner from "../../images/lapi.jpg"
 import React from 'react'
+import user from "../../icons/user.svg"
+import mail from "../../icons/mail.svg"
+import password from "../../icons/password.svg"
 
 function signup() {
   return (
     <div className="container-form">
         <div className="box" style={{
-            backgroundImage: `url(${bg})`
-        }}>
-            <button>Register now...</button>
-        </div>
-        <div className="box">
-
+      backgroundImage: `url(${banner})`
+  }}>
+          <div>
+              <h1>gn up</h1>
+              <form action="">
+                <input type="text" name="fname" placeholder="Enter your full name" style={{
+                  backgroundImage: `url(${user})`
+                }} />
+                <input type="epassword" name="email" placeholder="Enter your email" style={{
+                    backgroundImage: `url(${mail})`
+                }} />
+                <input type="password" name="password" placeholder="Enter your password" style={{
+                    backgroundImage: `url(${password})`
+                }} />
+                <input type="submit" value="Sign up" />
+                <span>Already have an account? <a href="/login">Log in</a></span>
+              </form>
+          </div>
+          <div>
+            <button>Welcome...</button>
+          </div>
         </div>
     </div>
   )
